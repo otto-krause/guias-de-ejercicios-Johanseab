700d01c049dc7d4de9c1aee164fcb11e4f7193c1
Johanseab <aguilarjohoan@gmail.com>
2020-04-11 19:22:51 -0300

    se usa un for para hacer las sumas sucesivas,y un if para saber si los numeros ingresados no son 0

--- Guia 1/E2/Program.cs
+++ Guia 1/E2/Program.cs
@@ -6,7 +6,27 @@ namespace E2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1=0,num2=0,Producto=0,resto=0,cociente=0;
+            Console.WriteLine("inserte dos numeros");
+            num1=Int32.Parse(Console.ReadLine());
+            num2=Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < num2; i++)
+            {
+                Producto=Producto+num1;
+            }
+           if (num1 > 0 && num2 > 0)
+           {
+               cociente = 0;
+               resto = num1;
+               while (resto >= num2)
+               {
+                   resto -= num2;
+                   cociente++;
+               }
+           }
+          Console.WriteLine("el producto es: "+Producto);
+          Console.WriteLine("el cociente es: "+cociente);
+          Console.WriteLine("el resto es: "+resto);
         }
     }
 }
