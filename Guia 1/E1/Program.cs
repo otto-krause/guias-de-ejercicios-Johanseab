e0e1ca7846da56f9e32821b5e9b104669e1d8ef4
Johanseab <aguilarjohoan@gmail.com>
2020-04-11 17:59:24 -0300

    se uso el metodo de borbujeo para ordenar el vector

--- Guia 1/E1/Program.cs
+++ Guia 1/E1/Program.cs
@@ -6,7 +6,44 @@ namespace E1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vector = new int[10];
+            int suma=0,promedio=0;
+            int menor=10000000;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ingrese un numero");
+                vector[i]= Int32.Parse(Console.ReadLine());
+                suma += vector[i];
+                promedio= suma/10;
+            }    
+           for (int i = 0; i < 10; i++)
+            {
+                    if(vector[i]<menor)
+                    {
+                        menor=vector[i];
+                    }
+             }
+           for (int i = 0; i < 10 ; i++)
+           {
+               for (int j = i+1 ; j < 10 ; j++)
+               {
+                   if(vector[j] > vector[i])
+                   {
+                       int aux;
+                       aux=vector[j];
+                       vector[j]=vector[i];
+                       vector[i]= aux;
+                   }
+               }
+           }
+            Console.WriteLine("la suma es: " +suma);   
+            Console.WriteLine("El promedio es de: "+promedio);
+            Console.WriteLine("El menor Elemento es: "+menor);
+            Console.WriteLine("orden de Mayor a Menor");
+            for (int i = 0; i < 10; i++)
+            {
+               Console.Write(" "+vector[i]); 
+            }
         }
     }
 }
