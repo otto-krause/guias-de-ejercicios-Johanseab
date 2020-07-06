6e6bc632964273ec0014d7fba4cdd9e44d83cfd2
Johanseab <aguilarjohoan@gmail.com>
2020-07-06 12:23:26 -0300

    Hice el 1

--- Guia 5/E1/Program.cs
+++ Guia 5/E1/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace E1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           List<Persona> personas=new List<Persona>();
+           Persona p1 = new Persona(30,"Juan","Perez");
+           Persona p2 = new Persona(15,"Juan","Rodrigez");
+           Persona p3 = new Persona(57,"Jose","Perez");
+           personas.Add(p1);
+           personas.Add(p2);
+           personas.Add(p3);
+
+           List<Persona>mayoresDeEdad=personas.Where(persona => persona.Edad >= 18).ToList();
+           Console.WriteLine("Mayores de Edad:\n");
+           mayoresDeEdad.ForEach(personas => Console.WriteLine("Nombre: " + personas.Nombre + "\nApellido: " + personas.Apelliodo + "\nEdad: " + personas.Edad));
+
+           List<Persona> Juanes=personas.Where(persona => persona.Nombre == "Juan").ToList();
+           Console.WriteLine("\nLas personas llamadas Juan:\n");
+           Juanes.ForEach(personas => Console.WriteLine("Nombre: " + personas.Nombre + "\nApellido: " + personas.Apelliodo + "\nEdad: " + personas.Edad));
+
+           List<Persona> Perez=personas.Where(personas => personas.Apelliodo == "Perez").ToList();
+           Console.WriteLine("\nLas personas con apellido Perez: \n");
+           Perez.ForEach(persona => Console.WriteLine("Nombre: " + persona.Nombre + "\nApellido: " + persona.Apelliodo + "\nEdad: " + persona.Edad));
         }
     }
 }
