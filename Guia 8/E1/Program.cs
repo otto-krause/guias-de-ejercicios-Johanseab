c0b1d2e93ec396f1589820c83fb88857078ce876
Johanseab <aguilarjohoan@gmail.com>
2020-11-20 13:26:51 -0300

    Hice el E1

---
+++ Guia 8/E1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pelicula;
+
+            Console.WriteLine("Ingresar el nombre de un pelicula");
+            pelicula=Console.ReadLine();
+
+            ComandosPeliculas.ingresarPeliculas(pelicula);
+            ComandosPeliculas.verTodasLasPeliculas(); 
+            ComandosPeliculas.verTodasLasPeliculasSinRepetir();
+        }
+    }
+}
